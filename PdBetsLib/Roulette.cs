8b9d78b750dd595f3bab8aed8d6aa5b153a98f9a
Victor L. Senior (VLS) <support@publicdomain.tech>
2016-08-31 00:56:24 -0400

    Add GetNumberColor to Roulette.cs

--- PdBetsLib/Roulette.cs
+++ PdBetsLib/Roulette.cs
@@ -25,6 +25,7 @@ namespace PdBets
     // Directives
     using System;
     using System.Collections.Generic;
+    using System.Drawing;
     using System.Runtime.InteropServices;
 
     /// <summary>
@@ -149,6 +150,66 @@ namespace PdBets
 
             // Validated correctly
             return true;
+        }
+
+        /// <summary>
+        /// Gets the color of the passed number.
+        /// </summary>
+        /// <returns>Color structure, corresponding to either Black, Red or Green.</returns>
+        /// <param name="number">The number to test.</param>
+        public Color GetNumberColor(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 9:
+                case 12:
+                case 14:
+                case 16:
+                case 18:
+                case 19:
+                case 21:
+                case 23:
+                case 25:
+                case 27:
+                case 30:
+                case 32:
+                case 34:
+                case 36:
+
+                    // Red number
+                    return Color.Red;
+
+                case 2:
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                case 11:
+                case 13:
+                case 15:
+                case 17:
+                case 20:
+                case 22:
+                case 24:
+                case 26:
+                case 28:
+                case 29:
+                case 31:
+                case 33:
+                case 35:
+
+                    // Black number
+                    return Color.Black;
+
+                default:
+
+                    // Anything else = green
+                    return Color.Green;
+            }
         }
 
         /// <summary>
